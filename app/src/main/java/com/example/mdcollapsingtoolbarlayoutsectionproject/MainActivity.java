a3a63337c2b06c85f17815d7ac96b0c40ec62210
DevStar999 <wolverinerage99@gmail.com>
2021-08-19 19:50:13 +0530

    Adding back action to the toolbar

--- app/src/main/java/com/example/mdcollapsingtoolbarlayoutsectionproject/MainActivity.java
+++ app/src/main/java/com/example/mdcollapsingtoolbarlayoutsectionproject/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
         initialise();
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
